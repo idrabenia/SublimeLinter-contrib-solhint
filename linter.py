f2a39768a5d0a63a916b67597ad7bc28f0f3fba9
Ilya Drabenia <ilya.drabenia@altoros.com>
2017-10-06 15:41:00 +0300

    Initial commit

---
+++ linter.py
@@ -0,0 +1,25 @@
+from SublimeLinter.lint import NodeLinter, util
+
+
+class Solhint(NodeLinter): 
+    syntax = 'solidity' 
+    cmd = 'solhint -f visualstudio unix @'
+    executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
+    regex = (
+        r'^.+?\((?P<line>\d+),(?P<col>\d+)\): '
+        r'(.?(?P<warning>warning)|(?P<error>error)).+?'
+        r':(?P<message>.+)'
+    )
+    multiline = True
+    line_col_base = (1, 1)
+    tempfile_suffix = 'solidity'
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = r'\s*/[/*]'
